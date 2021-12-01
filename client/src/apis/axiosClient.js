27ff02cdec4a45c21d82d1e427cc1b77c614db4a
NguyenHaiNam24082000 <nghainam2000@gmail.com>
2021-12-01 13:14:13 +0700

    update axios client

---
+++ client/src/apis/axiosClient.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+export const axiosClient = axios.create({
+  baseURL: "http://localhost:3000/api",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    "Access-Control-Allow-Headers":
+      "X-Requested-With, content-type, Authorization",
+  },
+});
+
+// Add a request interceptor
+axiosClient.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+axiosClient.interceptors.response.use(
+  function (response) {
+    // Do something with response data
+    return response.data;
+  },
+  function (error) {
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
